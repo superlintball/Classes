5f124727a748f345b5f7ae666e28974a993028a7
superlintball <ravnikator@gmail.com>
2017-10-11 11:15:28 -0700

    Started the classes project finally :/

---
+++ media.h
@@ -0,0 +1,18 @@
+// header file to build basic media
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Media
+{
+	public:
+		char getTitle[80];
+		int getYear;
+		int getType;
+	private:
+		char title[80];
+		int year;
+		int type;
+}
